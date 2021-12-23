fc957524df8f55235db0d5a0f6bf9136fe561dc4
ibrahim altay <ibrahimaltay152@hotmail.com>
2021-12-24 01:54:28 +0300

    Added block and unblock website functionality

--- app.py
+++ app.py
@@ -25,6 +25,14 @@ def execute_shell_command(command):
         except Exception as e:
             return e
 
+def block_domain(domain:str):
+   block_domain_command = f"sudo /bin/sh -c 'echo \"100.0.0.0 {domain}\" >> /etc/hosts'"
+   execute_shell_command(block_domain_command)
+
+def unblock_domain(domain:str):
+    unblock_domain_command = f"sudo /bin/sh -c \"sed '/youtube.com/d' /etc/hosts | sponge /etc/hosts\""
+    execute_shell_command(unblock_domain_command)
+
 import json
 with open('config.json') as f:
     credentials = json.load(f)
@@ -43,7 +51,17 @@ USER_ID = int(credentials['USER_ID'])
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
+
+
 admin_only = lambda message: message.from_user.id == USER_ID
+daily_limit = 15
+
+
+async def set_daily_limit(limit:int):
+    daily_limit = limit
+
+async def get_daily_limit():
+    return daily_limit
 
 @dp.message_handler(admin_only)
 async def echo(message: types.Message):
@@ -52,8 +70,20 @@ async def echo(message: types.Message):
         if text_array[0] == 'sendfile':
             send_file(text_array[1].strip(), EMAIL_ADDRESS, EMAIL_PASSWORD, text_array[1].strip())
             await message.answer('Emailed the file to you kind sir.')
-        elif text_array[0] == 'setlimit' and isinstance(text_array[1], int) and len(text_array) == 2:
+        
+        elif text_array[0] == 'setlimit' and isinstance(int(text_array[1]), int) and len(text_array) == 2:
+            await set_daily_limit(int(text_array[1]))
             await message.answer(f'Daily usage limit is set to {text_array[1]} minutes!')
+        
+        elif text_array[0] == 'getlimit' and len(text_array) == 1:
+            daily_limit = await get_daily_limit()
+            await message.answer(f'Daily usage limit is {daily_limit} minutes!')
+        
+        elif text_array[0] == 'blockdomain' and len(text_array) == 2:
+            block_domain(text_array[2])
+        
+        elif text_array[0] == 'unblockdomain' and len(text_array) == 2:
+            unblock_domain(text_array[2])
         else:
             output = execute_shell_command(message.text)
             if output:
@@ -62,7 +92,7 @@ async def echo(message: types.Message):
             await message.answer(e)
 
 if __name__ == '__main__':
-        time.sleep(10)
+        time.sleep(5)
         executor.start_polling(dp, skip_updates=True)
 
 # @dp.message_handler(commands=['start', 'help'])
